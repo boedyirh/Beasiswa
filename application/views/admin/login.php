0593cbfe55dba85f6df36b9a557de96efa000819
Boedy Irhadtanto <boedyirh@yahoo.co.id>
2018-11-14 07:49:29 +0700

    Add files via upload

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -77,17 +77,13 @@
 		<table align="center" style="margin-bottom: 0" class="table-form" width="90%">
 			<tr><td width="40%">Username</td><td><input type="text" autofocus name="u" required style="width: 200px" autofocus class="form-control"></td></tr>
 			<tr><td>Password</td><td><input type="password" name="p" required style="width: 200px" class="form-control"></td></tr>
-			<tr><td>Tahun</td><td><select name="ta" class="form-control" required><option value="">--</option>
-			<?php 
-			for ($i = 2017; $i <= (date('Y')); $i++) {
-				if (date('Y') == $i) {
-					echo "<option value='$i' selected>$i</option>";
-				} else {
-					echo "<option value='$i'>$i</option>";
-				}
-			}
-			?>
-			</select>
+			<tr><td>Tahun</td><td> 
+      
+      	 <?php
+           $PeriodeAktif='2018';
+ 		ComboBox("ta", "bsw_periode", "PeriodeID", "Tahun", 'xxx', "PeriodeID", "form-control","200px","----Periode-----");
+	 
+   ?>
 			</td></tr>
 			<tr><td></td><td><input type="submit" class="btn btn-success" value="Login"></td></tr>
 		</table>
